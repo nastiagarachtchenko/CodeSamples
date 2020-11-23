0594df9f33dfc5f3a20442cd5b0580795639eb81
nastiagarachtchenko <51330537+nastiagarachtchenko@users.noreply.github.com>
2020-11-22 16:01:08 -0800

    Add files via upload

---
+++ DeathZoneBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Rewired;
+
+public class DeathZoneBehaviour : MonoBehaviour {
+    public Player player;
+    public int playerId = 0;
+    public GameObject canvas;
+    public string currentScene;
+    public bool dead = false;
+
+
+    // Use this for initialization
+    void Start () {
+        player = ReInput.players.GetPlayer(playerId);
+        canvas.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (canvas.activeSelf && player.GetButton("resetSwitch")) {
+            SceneManager.LoadScene(currentScene);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Player") {
+            dead = true;
+            StartCoroutine(countdowntoReset(col));
+        }
+    }
+
+    public bool isDead() {
+        return dead;
+    }
+
+    IEnumerator countdowntoReset(Collider2D col) {
+        yield return new WaitForSeconds(.8f);
+        //col.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3 (0,0,0);
+        yield return new WaitForSeconds(2f);
+        canvas.SetActive(true);
+    }
+
+}
---
+++ iceBridgeBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class iceBridgeBehaviour : MonoBehaviour {
+    private Rigidbody2D rb2D;
+    public Player player;
+    public iceBridge ib;
+    private GameObject bridge;
+    public int playerId = 0;
+    public bool letGo = true;
+    private bool pressedDown = false;
+    private float rotationNum;
+    public AudioSource sound;
+    int lol = 0;
+
+
+    // Use this for initialization
+    void Start () {
+        sound = GameObject.Find("icebridgeaudio").GetComponent<AudioSource>();
+        ib = GameObject.Find("AimPoint").GetComponent<iceBridge>();
+        bridge = gameObject.transform.Find("IceBridge").gameObject;
+        rb2D = bridge.GetComponent<Rigidbody2D>();
+        player = ReInput.players.GetPlayer(playerId);
+        rotationNum = ib.returnAngle();
+
+        sound.Play();
+
+        transform.Rotate(0, 0, rotationNum*56); 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (letGo && player.GetButton("growbridge") && gameObject.transform.localScale.x < 1.5f) {
+            gameObject.transform.localScale += new Vector3(0.01f, 0, 0);
+            pressedDown = true;
+        } else if (pressedDown) {
+            if (lol == 0) {
+                sound.Pause();
+                lol++;
+            }
+            letGo = false;
+            rb2D.gravityScale = 1;
+            StartCoroutine(cooldown());
+            StartCoroutine(PowerUpcooldown());
+        }  
+    }
+
+    IEnumerator cooldown() {
+        yield return new WaitForSeconds(.9f);
+        rb2D.bodyType = RigidbodyType2D.Static;
+    }
+
+    IEnumerator PowerUpcooldown() {
+        ib.setPressable(false);
+        yield return new WaitForSeconds(2f);
+        ib.setPressable(true);
+    }
+
+}
